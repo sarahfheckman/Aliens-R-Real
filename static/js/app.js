d873d6e69cbbae8af9f5a8c521829f3c84aef0b8
SarahHeck <sfheckman@gmail.com>
2019-03-08 09:28:26 -0600

    table

--- static/js/app.js
+++ static/js/app.js
@@ -39,3 +39,54 @@ data.forEach((data) => {
     })
 
 });
+
+// use d3 select insert data into table 
+var tbody = d3.select("tbody");
+
+// Step 1: Loop Through `data` and console.log each alien report object
+data.forEach(function(alienReport) {
+    console.log(alienReport);
+  });
+  
+  // Step 2:  Use d3 to append one table row `tr` for each alien report object
+  data.forEach(function(alienReport) {
+    console.log(alienReport);
+    var row = tbody.append("tr");
+  });
+  
+  // Step 3:  Use `Object.entries` to console.log each alien report value
+  data.forEach(function(alienReport) {
+    console.log(alienReport);
+    var row = tbody.append("tr");
+  
+    Object.entries(alienReport).forEach(function([key, value]) {
+      console.log(key, value);
+    });
+  });
+  
+  // Step 4: Use d3 to append 1 cell per alien report value (datetime, city, state, country, shape, duration)
+  data.forEach(function(alienReport) {
+    console.log(alienReport);
+    var row = tbody.append("tr");
+  
+    Object.entries(alienReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the alien report object
+      var cell = tbody.append("td");
+    });
+  });
+  
+  // Step 5: Use d3 to update each cell's text with
+  // alien report values
+  data.forEach(function(alienReport) {
+    console.log(alienReport);
+    var row = tbody.append("tr");
+    Object.entries(alienReport).forEach(function([key, value]) {
+      console.log(key, value);
+      // Append a cell to the row for each value
+      // in the alien report object
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
